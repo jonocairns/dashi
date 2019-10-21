c85a11556f6ed3082b340c176292ef3a491104c2
Jono Cairns <jonocairns@gmail.com>
2019-10-22 10:48:05 +1300

    update dependancies

--- src/cards.js
+++ src/cards.js
@@ -1,8 +1,5 @@
 import plex from './assets/icons/plex.png';
-import plexpy from './assets/icons/plexpy.png';
 import request from './assets/icons/request.png';
-import discord from './assets/icons/discord.jpeg';
-import uptime from './assets/icons/uptime.png';
 
 const cards = [
     {
